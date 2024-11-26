0f7da83db152fedd7784a84823c5610de3568e07
phoss001 <phoss001@doc.gold.ac.uk>
2024-11-26 21:24:25 +0000

    tables for datas

--- createGame_db.sql
+++ createGame_db.sql
@@ -19,7 +19,7 @@ VALUES
     ('Valorant', 0),
     ('Minecraft', 19.99),
     ('The last of us', 15.99),
-    ('Fornite', 0),
+    ('Fortnite', 0),
     ('It takes two', 34.99),
     ('Marvels Spider-man', 49.55),
     ('Call of Duty: Black Ops 6', 53.99);
