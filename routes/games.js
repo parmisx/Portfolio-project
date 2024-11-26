5b315ad06e1b4ca36d952e307535dfef36c049e6
phoss001 <phoss001@doc.gold.ac.uk>
2024-11-26 17:15:08 +0000

    games api

--- routes/games.js
+++ routes/games.js
@@ -1,39 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const request = require("request");
-
-// // FreeToGame: Get all games
-// router.get('/games', function (req, res, next) {
-//     const url = 'https://www.freetogame.com/api/games';
-    
-//     request(url, (err, response, body) => {
-//         if (err || response.statusCode !== 200) {
-//             return res.send("Unable to fetch games. Please try again later.");
-//         }
-        
-//         const games = JSON.parse(body);
-//         res.render('games.ejs', { games, error: null });
-//     });
-// });
-
-// // FreeToGame: Get game by ID
-// router.get('/game/:id', function (req, res, next) {
-//     const gameId = req.params.id;
-//     const url = `https://www.freetogame.com/api/game?id=${gameId}`;
-    
-//     request(url, (err, response, body) => {
-//         if (err || response.statusCode !== 200) {
-//             return res.send("Unable to fetch game details. Please try again later.");
-//         }
-        
-//         const game = JSON.parse(body);
-//         res.render('game.ejs', { game, error: null });
-//     });
-// });
-
-// // Export the router
-// module.exports = router;
-
 const express = require("express")
 const router = express.Router()
 
@@ -96,3 +60,39 @@ router.get('/bargaingames', function(req, res, next) {
 
 // Export the router object so index.js can access it
 module.exports = router
+
+// const express = require("express");
+// const router = express.Router();
+// const request = require("request");
+
+// // FreeToGame: Get all games
+// router.get('/games', function (req, res, next) {
+//     const url = 'https://www.freetogame.com/api/games';
+    
+//     request(url, (err, response, body) => {
+//         if (err || response.statusCode !== 200) {
+//             return res.send("Unable to fetch games. Please try again later.");
+//         }
+        
+//         const games = JSON.parse(body);
+//         res.render('games.ejs', { games, error: null });
+//     });
+// });
+
+// // FreeToGame: Get game by ID
+// router.get('/game/:id', function (req, res, next) {
+//     const gameId = req.params.id;
+//     const url = `https://www.freetogame.com/api/game?id=${gameId}`;
+    
+//     request(url, (err, response, body) => {
+//         if (err || response.statusCode !== 200) {
+//             return res.send("Unable to fetch game details. Please try again later.");
+//         }
+        
+//         const game = JSON.parse(body);
+//         res.render('game.ejs', { game, error: null });
+//     });
+// });
+
+// // Export the router
+// module.exports = router;
